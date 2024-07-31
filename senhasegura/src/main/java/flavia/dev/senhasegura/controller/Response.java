a8202de2f92eec8998279b414eab1512043c2265
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-30 22:08:36 -0300

    response de falha criado

---
+++ senhasegura/src/main/java/flavia/dev/senhasegura/controller/Response.java
@@ -0,0 +1,7 @@
+package flavia.dev.senhasegura.controller;
+
+import java.util.List;
+
+public record Response(List<String> falhou) {
+
+}
--- senhasegura/src/main/java/flavia/dev/senhasegura/controller/senhaController.java
+++ senhasegura/src/main/java/flavia/dev/senhasegura/controller/senhaController.java
@@ -2,13 +2,14 @@ package flavia.dev.senhasegura.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class senhaController {
 	
 	@PostMapping(name = "/validar")
-	public ResponseEntity<?> validarSenha(){
+	public ResponseEntity<?> validarSenha(@RequestBody Request request){
 		return ResponseEntity.noContent().build();
 	}
 
