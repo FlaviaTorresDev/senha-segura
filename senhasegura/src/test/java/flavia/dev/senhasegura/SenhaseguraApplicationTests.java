44c5483bee7fb74de2708f33176b2f1ade33a9e6
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-30 21:56:12 -0300

    Inicializando projeto

---
+++ senhasegura/src/test/java/flavia/dev/senhasegura/SenhaseguraApplicationTests.java
@@ -0,0 +1,13 @@
+package flavia.dev.senhasegura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenhaseguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
